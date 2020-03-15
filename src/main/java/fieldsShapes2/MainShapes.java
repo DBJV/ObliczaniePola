df83290c480f462a49b184aec80655197e0196d0
DBJV <dawid.bednarek94@gmail.com>
2020-03-15 18:36:35 +0100

    CalctulateField with "for" - na piechote...

--- src/main/java/fieldsShapes2/MainShapes.java
+++ src/main/java/fieldsShapes2/MainShapes.java
@@ -14,10 +14,32 @@ public class MainShapes {
         tablica[1].calculateField(3, 4);
 
         System.out.println(" ************************");
+        System.out.println("Pierwszy sposob:");
 
         for (int i = 0; i < tablica.length; i++) {
             tablica[i].calculateField(i, 2);
             System.out.println(i);
+        }
+        System.out.println("Dla indexu 0 tablicy");
+        for (int i = 0; i < tablica.length; i++) {
+            tablica[0].calculateField(i, 2);
+        }
+        System.out.println("Dla indexu 1 tablicy");
+
+        for (int i = 0; i < tablica.length; i++) {
+            tablica[1].calculateField(i, 2);
+        }        System.out.println("Dla indexu 2 tablicy");
+
+        for (int i = 0; i < tablica.length; i++) {
+            tablica[2].calculateField(i, 2);
+        }        System.out.println("Dla indexu 3 tablicy");
+
+        for (int i = 0; i < tablica.length; i++) {
+            tablica[3].calculateField(i, 2);
+        }        System.out.println("Dla indexu 4 tablicy");
+
+        for (int i = 0; i < tablica.length; i++) {
+            tablica[4].calculateField(i, 2);
         }
     }
 }
